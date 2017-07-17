bf326c5ae5adadba81e64b63c4948db796cb2cf8
AQP20171679 <alejandraadriantejada@gmail.com>
2017-07-17 13:35:00 -0500

    Base de para peso en otros planetas

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var masa= parseInt(prompt("Ingrese su masa: "));
+var planeta= prompt("En que planeta?");
+
+if(planeta=="Mercurio")
+	alert("Tu peso es: "+ masa*0.38);
+else if(planeta=="Venus")
+	alert("Tu peso es: "+ masa*0.91);
+else if(planeta=="Marte")
+	alert("Tu peso es: "+ masa*0.38);
+else if(planeta=="Tierra")
+	alert("Tu peso es: "+ masa);
+else if(planeta=="Jupiter")
+	alert("Tu peso es: "+ masa*2.54);
+else if(planeta=="Saturno")
+	alert("Tu peso es: "+ masa*1.08);
+else if(planeta=="Urano")
+	alert("Tu peso es: "+ masa*0.91);
+else if(planeta=="Neptuno")
+	alert("Tu peso es: "+ masa*1.19);
+else if(planeta=="Pluton")
+	alert("Tu peso es: "+ masa*0.06);
