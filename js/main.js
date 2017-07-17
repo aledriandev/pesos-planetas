d9262d1e629bb047cf0c21d0d30e6bfa6dac1d8c
bariflor <barihuamachuco@gmail.com>
2017-07-17 14:05:14 -0500

    se agrego funcion

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-var masa= parseInt(prompt("Ingrese su masa: "));
-var planeta= prompt("En que planeta?");
-
-if(planeta=="Mercurio")
+var masa= parseInt(prompt("Ingrese su masa: ", "50"));
+var planeta= prompt("En que planeta?", "Mercurio");
+function pesoPlanetas(masa,planeta){
+	if(planeta=="Mercurio")
 	alert("Tu peso es: "+ masa*0.38);
-else if(planeta=="Venus")
+	else if(planeta=="Venus")
 	alert("Tu peso es: "+ masa*0.91);
-else if(planeta=="Marte")
+	else if(planeta=="Marte")
 	alert("Tu peso es: "+ masa*0.38);
-else if(planeta=="Tierra")
+	else if(planeta=="Tierra")
 	alert("Tu peso es: "+ masa);
-else if(planeta=="Jupiter")
+	else if(planeta=="Jupiter")
 	alert("Tu peso es: "+ masa*2.54);
-else if(planeta=="Saturno")
+	else if(planeta=="Saturno")
 	alert("Tu peso es: "+ masa*1.08);
-else if(planeta=="Urano")
+	else if(planeta=="Urano")
 	alert("Tu peso es: "+ masa*0.91);
-else if(planeta=="Neptuno")
+	else if(planeta=="Neptuno")
 	alert("Tu peso es: "+ masa*1.19);
-else if(planeta=="Pluton")
+	else if(planeta=="Pluton")
 	alert("Tu peso es: "+ masa*0.06);
+
+}
+pesoPlanetas(masa,planeta);
